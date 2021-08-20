f7cfcff6c6f7785b780d870230c6e852068ce651
Kim-Yonghwi <yhkim@seoulstore.com>
2021-08-20 23:48:44 +0900

    feat: 메타테이블 엿보기

--- README.md
+++ README.md
@@ -6,6 +6,15 @@
 - **Step**
   - 실제 작업을 수행하는 역할로 Batch로 실제 처리하고자 하는 기능과 설정을 모두 포함한다.
   - `Step` 안에는 `Tasklet` 혹은 `Reader & Processor & Writer` 묶음이 존재하는데 하나의 `Step` 안에서 두개가 동시에 사용될 순 없다.
+- Meta tables
+  - BATCH_JOB_INSTANCE
+    - `Job`이 실행될 때 생성되는 `JobInstance`에 관한 정보를 저장하고 있다.
+    - `Job`이 실행될 때마다 해당 테이블에 기록되진 않고 `Job Parameter`(외부에서 전달 받는 파라미터)에 따라서 해당 테이블에 데이터가 저장되는데 이미 동일한 파라미터로 실행된 `Job`이 성공한 경우에는 테이블에 데이터가 저장되진 않는다.
+  - BATCH_JOB_EXECUTION
+    - `Job` 실행에 대한 성공/실패 여부등 `Job` 실행에 대한 내역을 가지고 있다.
+  - BATCH_JOB_EXECUTION_PARAMS
+    - `BATCH_JOB_EXECUTION` 테이블이 생성될 당시에 입력 받은 `Job Parameter` 정보를 가지고 있다. 
 
 ### Index
-- [Spring Batch 프로젝트 생성하기](https://jojoldu.tistory.com/325?category=902551)
+- [1. Spring Batch 프로젝트 생성하기](https://jojoldu.tistory.com/325?category=902551)
+- [2. 메타테이블 엿보기](https://jojoldu.tistory.com/326?category=902551)
--- src/main/kotlin/com/seoulstore/batch/job/SimpleJobConfiguration.kt
+++ src/main/kotlin/com/seoulstore/batch/job/SimpleJobConfiguration.kt
@@ -5,9 +5,11 @@ import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
 import org.springframework.batch.core.StepContribution
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
+import org.springframework.batch.core.configuration.annotation.JobScope
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
 import org.springframework.batch.core.scope.context.ChunkContext
 import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -23,17 +25,38 @@ class SimpleJobConfiguration(
     fun simpleJob(): Job {
         // simpleJob 이란 이름의 Job 생성
         return jobBuilderFactory.get("simpleJob")
-            .start(simpleStep1())
+            .start(simpleStep1(null))
+            .next(simpleStep2(null))
             .build()
     }
 
     @Bean
-    fun simpleStep1(): Step {
+    @JobScope
+    fun simpleStep1(
+        @Value("#{jobParameters[requestDate]}") requestDate: String?
+    ): Step {
         // simpleStep1 이란 이름의 Step 생성
         return stepBuilderFactory["simpleStep1"]
             // tasklet: step 안에서 수행될 기능을 명시
             .tasklet { contribution: StepContribution?, chunkContext: ChunkContext? ->
-                log.info(">>>>> This is Step1")
+                log.info(">>>>> This is Step1");
+                log.info(">>>>> requestDate = {}", requestDate);
+                RepeatStatus.FINISHED
+            }
+            .build()
+    }
+
+    @Bean
+    @JobScope
+    fun simpleStep2(
+        @Value("#{jobParameters[requestDate]}") requestDate: String?
+    ): Step {
+        // simpleStep1 이란 이름의 Step 생성
+        return stepBuilderFactory["simpleStep2"]
+            // tasklet: step 안에서 수행될 기능을 명시
+            .tasklet { contribution: StepContribution?, chunkContext: ChunkContext? ->
+                log.info(">>>>> This is Step2");
+                log.info(">>>>> requestDate = {}", requestDate);
                 RepeatStatus.FINISHED
             }
             .build()
